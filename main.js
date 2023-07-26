512a0f8de2ffac892db62dcbcd74f96230cab16d
The-XGhosT <ilyasqabbal@yahoo.com>
2023-07-26 11:30:36 +0100

    Iphone Product

---
+++ main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.image-main');
+let container = document.querySelector('.container');
+function change(phone)
+{
+    img.src = phone;
+
+}
+
+function colors(color){
+    container.style.background=color;
+}
